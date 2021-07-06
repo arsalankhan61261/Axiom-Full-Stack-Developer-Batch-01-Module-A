41df113624a12812c552f715ee2c1cb1907d1a18
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-07-06 03:10:19 -0700

    Update script.js

--- Project 05/script.js
+++ Project 05/script.js
@@ -16,7 +16,41 @@ async function getRandomUser() {
     // Wait for response to convert into JSON
     const data = await res.json();
 
-    console.log(data);
+    // console.log(data);
+
+    // Get the User data
+    const user = data.results[0];
+    console.log(user);
+
+    // Create the New User
+    const newUser = {
+        name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+        balance: Math.floor(Math.random()*1000000)
+    }
+    console.log(newUser);
+
+    // Add the new user into the Data Array
+    addData(newUser);
 };
 
+// Function to add user data into user data array
+function addData(newUser) {
+    // Add the new User data into the user data array
+    data.push(newUser);
+    console.log('Data Array',data);
+    // Update the DOM to display users in the data array
+    updateDOM();
+}
+
+// Update the UI with data from the user data array
+function updateDOM(userData = data) {
+    // Clear previous UI
+    main.innerHTML = '<h2><strong>User</strong> Wealth</h2>'
+    // Loop through user data and render in the UI
+    userData.forEach();
+}
+
+// Create a Random User
+getRandomUser();
+getRandomUser();
 getRandomUser();
