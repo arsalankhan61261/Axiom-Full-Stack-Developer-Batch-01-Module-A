449911b5868cceaddfd9070524c83f738269d302
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-07-31 21:09:49 -0700

    Update script.js

--- Project 08/script.js
+++ Project 08/script.js
@@ -3,7 +3,6 @@ const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const generate = document.getElementById('generate');
 const resultsHeading = document.getElementById('results-heading');
-const resultsHeading = document.getElementById('results-heading');
 const meals = document.getElementById('meals');
 const selectedMeal = document.getElementById('selected-meal');
 
@@ -11,9 +10,16 @@ const selectedMeal = document.getElementById('selected-meal');
 function searchMeal(e) {
     //Prevent form submission and redirect
     e.preventDefault();
+    // Get the value from search input field
+    const searchText = search.value;
+    // Check if search input field is empty
+    if (searchText.trim()) {
+        console.log(searchText);
+    } else {
+        alert('Please enter search keyword')
+    };
 };
-
 
 // Event Listeneres
 // 1. Listen for form Submit
-submit.addEventListener('submit', searchMeal)
+submit.addEventListener('submit', searchMeal);
