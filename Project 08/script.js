232b1efb746277e4dc134ecde4ed08b75006ac21
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-07-31 20:55:51 -0700

    Update script.js

---
+++ Project 08/script.js
@@ -0,0 +1,19 @@
+// DOM Elements
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const generate = document.getElementById('generate');
+const resultsHeading = document.getElementById('results-heading');
+const resultsHeading = document.getElementById('results-heading');
+const meals = document.getElementById('meals');
+const selectedMeal = document.getElementById('selected-meal');
+
+// Function to search the meal using the API
+function searchMeal(e) {
+    //Prevent form submission and redirect
+    e.preventDefault();
+};
+
+
+// Event Listeneres
+// 1. Listen for form Submit
+submit.addEventListener('submit', searchMeal)
