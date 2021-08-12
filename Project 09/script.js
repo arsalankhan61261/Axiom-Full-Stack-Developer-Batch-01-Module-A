cd7d441a6ed2f9d02ea82ed1493f03d29f8c2247
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-12 01:47:38 -0700

    Update script.js

---
+++ Project 09/script.js
@@ -0,0 +1,15 @@
+// Get DOM Elements
+const balance = document.getElementById('balance');
+const moneyCredit = document.getElementById('money-credit');
+const moneyDebit = document.getElementById('money-debit');
+const lsit = document.getElementById('list');
+const form = document.getElementById('add-form');
+const reason = document.getElementById('reason');
+const amount = document.getElementById('amount');
+
+const Transactions = [
+    { id: 1, reason: 'Salary', amount: 5000 },
+    { id: 2, reason: 'Dinner', amount: 2000 },
+    { id: 3, reason: 'Dinner', amount: 2000 },
+    { id: 4, reason: 'Dinner', amount: 2000 },
+];
