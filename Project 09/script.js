9da9cb14ea72a79c8d7d81ac73bfce1aea7eb756
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-13 02:04:46 -0700

    Update script.js

--- Project 09/script.js
+++ Project 09/script.js
@@ -28,8 +28,8 @@ function displayTransaction(transaction) {
     transactionLI.classList.add( transaction.amount > 0 ? 'credit' : 'debit' );
     // Assign the inner HTML for the transaction li
     transactionLI.innerHTML = `
-        ${transaction.reason} <span>${type}${transaction.amount}</span>
-        <button class="delete-btn">X</button>   
+        ${transaction.reason} <span>${transaction.amount}</span>
+        <button class="delete-btn" onclick="deleteTransaction(${transaction.id})">X</button>   
     `;
     // Add the list item in the DOM under the transaction history list
     list.appendChild(transactionLI);
@@ -41,7 +41,82 @@ function init() {
     list.innerHTML = '';
     // Display all transactions in data base in the DOM
     transactions.forEach(displayTransaction);
+    // Update all balance values
+    updateBalance();
 };
 
 // Initialize the application
 init();
+
+// Function to update all balances
+function updateBalance() {
+    // Create a new array with just the amounts from the transactions array
+    const transactionAmounts = transactions.map( transaction => transaction.amount);
+    // console.log(transactions);
+    // console.log(transactionAmounts);
+    
+    // Calculate balance value
+    const totalBalance = transactionAmounts.reduce( (acc, amount) => ( acc += amount ), 0 );
+    // console.log(totalBalance);
+
+    // Calculate total Credit Balance
+    const creditBalance = transactionAmounts
+                            .filter(amount => amount > 0)
+                            .reduce( (acc, amount) => ( acc += amount ), 0 );
+    // console.log(creditBalance);
+
+    // Calculate total Debit Balance
+    const debitBalance = transactionAmounts
+                            .filter(amount => amount < 0)
+                            .reduce( (acc, amount) => ( acc += amount ), 0 );
+    // console.log(debitBalance);
+
+    // Update values in the DOM for overall balance, credit balance and debit balance
+    balance.innerText = `$${totalBalance}`;
+    moneyCredit.innerText = `$${creditBalance}`;
+    moneyDebit.innerText = `$${debitBalance}`;
+};
+
+// Function to create a random ID
+function createID() {
+    return Math.floor(Math.random() * 100000000000);
+};
+
+// Function to add a transaction from the form
+function addTransaction(e) {
+    // Stop page reloading on click add transaction button
+    e.preventDefault();
+    // Check if form has valid data
+    if ( reason.value.trim() === '' || amount.value.trim() === '' ) {
+        // Display error message if form is not complete
+        alert('Please provide a valid reason and transaction amount')
+    } else {
+        // Create an object for the transaction containing id,
+        // text for the reason and the transaction amount
+        const transaction = {
+            id: createID(),
+            reason: reason.value,
+            amount: +amount.value
+        }
+        // Push the new transaction into the transactions array
+        transactions.push(transaction);
+        // Display the new transaction in the DOM
+        displayTransaction(transaction);
+        // Update all balances
+        updateBalance();
+        // Clear form fields
+        reason.value = '';
+        amount.value = ''; 
+    }
+};
+
+// Function to delete a transaction from the history
+function deleteTransaction(id) {
+    transactions = transactions.filter( transaction => transaction.id !== id );
+    // Initialize the app again to update the DOM
+    init(); 
+};
+
+// Event Listeners
+// 1. Listen for form submit to add a transaction
+form.addEventListener('submit', addTransaction);
