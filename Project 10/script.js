66bd4ba1ba74248c373d031f2e36a20ee3ccbb1a
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-16 01:59:33 -0700

    Update script.js

--- Project 10/script.js
+++ Project 10/script.js
@@ -7,3 +7,12 @@ const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
 const title = document.getElementById('song-title');
 const albumArt = document.getElementById('album-art');
+
+// Songs Array
+const tracks = ['Monolink return to OZ', 'Jay Aliyev Move On'];
+
+// Index of currently playing songs
+let trackIndex = 1;
+
+// Load the initial track
+loadTrack(tracks[trackIndex]);
