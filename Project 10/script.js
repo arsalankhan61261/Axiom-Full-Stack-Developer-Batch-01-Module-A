b231ee99b2b221d09a425178845d6e2554f750d7
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-16 02:14:13 -0700

    Update script.js

--- Project 10/script.js
+++ Project 10/script.js
@@ -9,10 +9,20 @@ const title = document.getElementById('song-title');
 const albumArt = document.getElementById('album-art');
 
 // Songs Array
-const tracks = ['Monolink return to OZ', 'Jay Aliyev Move On'];
+const tracks = ['monolink return to OZ', 'Jay Aliyev Move On'];
 
 // Index of currently playing songs
 let trackIndex = 1;
 
 // Load the initial track
 loadTrack(tracks[trackIndex]);
+
+// Function to load the selected track
+function loadTrack(track) {
+    // Update the text for the title of track
+    title.innerText = track;
+    // Update the src in the audio element with the audio file of the selected track
+    audio.src = `Music/${track}.mp3`;
+    // Update the src in the image element with the image file of the selected track
+    albumArt.src = `Images/${track}.jpg`;
+};
