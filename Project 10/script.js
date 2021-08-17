4fafc6505750ccba375aa0229cdc960daa4af3ad
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-17 02:56:13 -0700

    Update script.js

--- Project 10/script.js
+++ Project 10/script.js
@@ -26,3 +26,76 @@ function loadTrack(track) {
     // Update the src in the image element with the image file of the selected track
     albumArt.src = `images/${track}.jpg`;
 };
+
+// Function to play the track
+function playTrack() {
+    // Add the second class 'play' to the container
+    container.classList.add('play');
+    // Romove the play icon and display the pause icon instead
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    // Play the track using the audio element
+    audio.play();
+};
+
+// Function to pause the track
+function pauseTrack() {
+    // Remove the second class 'play' from the container
+    container.classList.remove('play');
+    // Romove the pause icon and display the play icon instead
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    // Pause the track using the audio element
+    audio.pause();
+};
+
+// Fucntion to switch to previous track
+function prevTrack() {
+    // Decrement the value of trackIndex by 1 to select the previous track from the trackArray
+    trackIndex--;
+    // Check if selected track index is greater than the index of last
+    if ( trackIndex < 0 ) {
+        // Reassign the trackIndex to last track in the trackArray
+        trackIndex = tracks.length - 1;
+    };
+    // console.log(trackIndex);
+    // Load the selected track
+    loadTrack(tracks[trackIndex]);
+    // Play the selected track
+    playTrack();
+};
+
+// Fucntion to switch to next track
+function nextTrack() {
+    // Increment the value of trackIndex by 1 to select the next track from the trackArray
+    trackIndex++;
+    // Check if selected track index is greater than the index of last track
+    if ( trackIndex > tracks.length - 1 ) {
+        // Reassign the trackIndex to last track in the trackArray
+        trackIndex = 0;
+    };
+    // console.log(trackIndex);
+    // Load the selected track
+    loadTrack(tracks[trackIndex]);
+    // Play the selected track
+    playTrack();
+};
+
+// Event Listeners
+// 1. Listen for click on the play button
+playBtn.addEventListener('click', () => {
+    // Check if track is playing
+    const isPlaying = container.classList.contains('play');
+    // Conditional statement based on status of audio playback
+    if (isPlaying) {
+        // If the track is playing, pause the track
+        pauseTrack();
+    } else {
+        // If the track is not playing, play the track
+        playTrack(); 
+    }
+});
+
+// 2. Listen for click on the previousBtn
+previousBtn.addEventListener('click', prevTrack);
+
+// 3. Listen for click on the nextBtn
+nextBtn.addEventListener('click', nextTrack);
