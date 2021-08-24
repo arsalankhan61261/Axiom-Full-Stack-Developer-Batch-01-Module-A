5cf2243041a00e84d6544773d5b7e1ae5cc4c5cb
arsalankhan61261 <82482072+arsalankhan61261@users.noreply.github.com>
2021-08-24 00:45:24 -0700

    Update script.js

--- Project 11/script.js
+++ Project 11/script.js
@@ -9,9 +9,10 @@ let page = 1;
 
 // Function to asynchronously fetch posts from API
 async function fetchPosts() {
-    // Fetch posts from the JSON placeholder API
+    // Fetch posts from the JSON Placeholder API
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
     const data = await res.json();
+
     console.log(data);
     return data;
 };
